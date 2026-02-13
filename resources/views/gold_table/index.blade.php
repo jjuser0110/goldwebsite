d4e97b735ce351ba4c271c00715c576b67867a19
jjuser0110 <163217224+jjuser0110@users.noreply.github.com>
2026-02-14 06:24:27 +0800

    aaa

--- resources/views/gold_table/index.blade.php
+++ resources/views/gold_table/index.blade.php
@@ -48,16 +48,15 @@
     @endsection
     @section('scripts')
     <script>
-    $(function () {
-        $('#mytable').DataTable({
-            responsive: true,
-            paging: false,     
-            searching: false,  
-            info: false,
-            ordering: false,  
+    $(function(){
+        var table = $('#mytable').DataTable({
+            dom: '<"row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6 d-flex justify-content-center justify-content-md-end"f>><"table-responsive"t><"row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+            pageLength: 10,
+            displayLength: 5,
+            ordering:false,
+            lengthMenu: [5, 10, 25, 50, 75, 100],
         });
     });
-
     function saveData(id){
         var additional_value = $('#additional_value_'+id).val();
         $.ajax({
