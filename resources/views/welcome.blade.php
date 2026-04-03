5be4a1d46232df986f5db2672e7dda3d865b9171
jjuser0110 <163217224+jjuser0110@users.noreply.github.com>
2026-04-03 15:49:02 +0800

    aaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -653,7 +653,7 @@
             <button class="mobile-menu-btn" onclick="toggleMenu()">☰</button>
             <ul class="nav-menu" id="navMenu">
                 <li>
-                    <a>
+                    <a href="weixin://" target="_blank">
                         <span data-en="Wechat" data-cn="Wechat" data-bm="Wechat">
                             Wechat ID: Emas-6868
                         </span>
