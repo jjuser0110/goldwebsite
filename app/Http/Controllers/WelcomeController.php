5ce929c8154bf248575adcc67c13bf7de7cddadb
jjuser0110 <163217224+jjuser0110@users.noreply.github.com>
2026-04-19 10:57:24 +0800

    aa

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -32,63 +32,63 @@ class WelcomeController extends Controller
         return view('welcome', compact('now_date', 'now_time', 'goldRates'));
     }
 
-    public function getPrices(Request $request)
-    {
-        $goldRates = GoldTable::where('type', '!=', 'datetime')->get();
-
-        $data = [];
-        $name = [];
-
-        foreach ($goldRates as $row) {
-            $data[$row->type] = $row->new_value ?? 0.00;
-            $name[$row->type] = $row->show_name ?? $row->type;
-        }
-
-        return response()->json([
-            'status' => true,
-            'now_date' => now()->format('j F Y'),
-            'now_time' => now()->format('H:i:s'),
-            'data' => $data,
-            'name' => $name,
-        ]);
-    }
-    
-    // public function getPrices(Request $request){
-    //     $goldTypes = [
-    //         'pamp','goldbar','gold999','gold950',
-    //         'gold916','gold835','gold750','gold585','gold375','type1','type2','type3','type4','type5'
-    //     ];
+    // public function getPrices(Request $request)
+    // {
+    //     $goldRates = GoldTable::where('type', '!=', 'datetime')->get();
 
     //     $data = [];
     //     $name = [];
 
-    //     foreach ($goldTypes as $type) {
-    //         $goldrate = GoldTable::where('type',$type)->first()->new_value??0.00;
-
-    //         $start = Carbon::now()->subSeconds(5);
-    //         $end   = Carbon::now();
-    
-    //         $latest = DB::table('daily_rates')
-    //             ->where('type', $type)
-    //             ->whereBetween('datetime', [$start, $end])
-    //             ->orderBy('datetime', 'desc') 
-    //             ->whereNull('deleted_at')
-    //             ->first();
-                
-    //         $data[$type] = $latest ? $latest->rate : $goldrate;
-    //         $name[$type] = GoldTable::where('type',$type)->first()->show_name??$type;
+    //     foreach ($goldRates as $row) {
+    //         $data[$row->type] = $row->new_value ?? 0.00;
+    //         $name[$row->type] = $row->show_name ?? $row->type;
     //     }
-    //     $now_date = Carbon::now()->format('j F Y');
-    //     $now_time = Carbon::now()->format('H:i:s');
-    //     // dd($data);
+
     //     return response()->json([
     //         'status' => true,
-    //         'now_date' => $now_date,
-    //         'now_time' => $now_time,
+    //         'now_date' => now()->format('j F Y'),
+    //         'now_time' => now()->format('H:i:s'),
     //         'data' => $data,
     //         'name' => $name,
     //     ]);
     // }
+    
+    public function getPrices(Request $request){
+        $goldTypes = [
+            'pamp','goldbar','gold999','gold950',
+            'gold916','gold835','gold750','gold585','gold375','type1','type2','type3','type4','type5'
+        ];
+
+        $data = [];
+        $name = [];
+
+        foreach ($goldTypes as $type) {
+            $goldrate = GoldTable::where('type',$type)->first()->new_value??0.00;
+
+            $start = Carbon::now()->subSeconds(5);
+            $end   = Carbon::now();
+    
+            $latest = DB::table('daily_rates')
+                ->where('type', $type)
+                ->whereBetween('datetime', [$start, $end])
+                ->orderBy('datetime', 'desc') 
+                ->whereNull('deleted_at')
+                ->first();
+                
+            $data[$type] = $latest ? $latest->rate : $goldrate;
+            $name[$type] = GoldTable::where('type',$type)->first()->show_name??$type;
+        }
+        $now_date = Carbon::now()->format('j F Y');
+        $now_time = Carbon::now()->format('H:i:s');
+        // dd($data);
+        return response()->json([
+            'status' => true,
+            'now_date' => $now_date,
+            'now_time' => $now_time,
+            'data' => $data,
+            'name' => $name,
+        ]);
+    }
 
     
 }
